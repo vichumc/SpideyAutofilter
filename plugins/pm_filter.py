0fc86874d682c345f27c5fc01628e6082b318dca
vichumc <100103080+vichumc@users.noreply.github.com>
2022-11-28 21:57:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -825,6 +825,8 @@ async def manual_filters(client, message, text=False):
                                 reply_text, 
                                 disable_web_page_preview=True,
                                 reply_to_message_id=reply_id)
+                            await asyncio.sleep(30)
+                            await dd.edit(f"\n \n⚙️ Result  Closed ️")
                         else:
                             button = eval(btn)
                             await client.send_message(
@@ -834,6 +836,8 @@ async def manual_filters(client, message, text=False):
                                 reply_markup=InlineKeyboardMarkup(button),
                                 reply_to_message_id=reply_id
                             )
+                            await asyncio.sleep(30)
+                            await mm.edit(f"\n \n⚙️ Result  Closed ️")
                     elif btn == "[]":
                         await client.send_cached_media(
                             group_id,
